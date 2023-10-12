7216998c0dcca86a3da2026d735409100e751851
David Olanipekun <olaide.olanipekun@yahoo.com>
2023-10-13 00:29:36 +0100

    feat: authentification endpoints

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    db: {
-        port: Number(process.env.DB_PORT) || 5432,
-    },
-};
-exports.default = config;
-//# sourceMappingURL=config.js.map
